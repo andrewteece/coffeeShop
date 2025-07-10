b6363136c9b126cfed67dac1414b07b0e1e2bfd6
andrewteece <andrewteece@gmail.com>
2025-07-10 12:31:21 -0500

    import theme and provider into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import '@fontsource/roboto';
 import './App.css';
+import theme from './assets/theme';
+import { ThemeProvider } from '@mui/material';
 import Navbar from './components/layout/Navbar';
 
 function App() {
-  return <Navbar />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Navbar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
