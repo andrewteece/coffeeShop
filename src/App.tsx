6d26da76724b304572aa709911d6751c3c07f7be
andrewteece <andrewteece@gmail.com>
2025-07-10 14:47:03 -0500

    install framer motion, update Hero

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import theme from './assets/theme';
 import { ThemeProvider } from '@mui/material';
 import Navbar from './components/layout/Navbar';
+import Hero from './components/features/Hero';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Navbar />
+      <Hero />
     </ThemeProvider>
   );
 }
---
+++ src/components/features/Hero.tsx
@@ -0,0 +1,83 @@
+import { Typography, Box, Stack, Button, Container } from '@mui/material';
+import Espresso from '../../assets/images/espresso-coffee.png';
+import { motion } from 'framer-motion';
+
+export default function Hero() {
+  return (
+    <Container id='home' maxWidth='lg'>
+      <Stack
+        direction={{ xs: 'column', md: 'row' }}
+        spacing={2}
+        sx={{
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '85vh',
+        }}
+      >
+        <Box
+          sx={{
+            width: { xs: '100%', md: '50%' }, // Ensure this Box takes 50% of the width
+            py: 6,
+            textAlign: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            alignItems: 'center',
+          }}
+        >
+          <motion.div
+            initial={{ opacity: 0, y: -40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
+          >
+            <Typography
+              variant='h1'
+              sx={{ fontSize: { xs: '3rem', md: '4rem' } }}
+            >
+              Coffee For All
+            </Typography>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3, duration: 0.6 }}
+          >
+            <Typography
+              variant='h4'
+              sx={{ fontSize: { xs: '1rem', md: '1.5rem' } }}
+            >
+              The coffee revolution starts now.
+            </Typography>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.6, duration: 0.4 }}
+          >
+            <Button variant='contained' color='secondary' href='#coffee'>
+              Order Now
+            </Button>
+          </motion.div>
+        </Box>
+
+        <Box
+          sx={{
+            width: '50%', // Ensure this Box takes 50% of the width
+            padding: 2,
+            textAlign: 'center',
+          }}
+        >
+          <img
+            src={Espresso}
+            alt='espresso coffee drink'
+            loading='lazy'
+            style={{ width: '100%' }}
+          />
+        </Box>
+      </Stack>
+    </Container>
+  );
+}
