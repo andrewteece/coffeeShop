17332d632b8d4d3abd3fc18c3d5fe970a86e6f47
andrewteece <andrewteece@gmail.com>
2025-07-12 07:29:32 -0500

    wrapp App in ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+// define a theme with built-in colorSchemes support
+const theme = createTheme({
+  colorSchemes: {
+    light: { palette: { mode: 'light' } },
+    dark: { palette: { mode: 'dark' } },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme} defaultMode='light'>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
